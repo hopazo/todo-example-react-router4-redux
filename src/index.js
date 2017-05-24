eb06ac411dd7cb056dd4a1d1bd49ad9994710282
Hector Opazo <hopazo@pred.cl>
2017-05-24 16:38:20 -0400

    Incluir react-router-dom como dependencia

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {render} from "react-dom";
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
 import createHistory from "history/createBrowserHistory";
-import {routerMiddleware} from "react-router-redux";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import {Switch, Route} from 'react-router-dom';
+import {ConnectedRouter, routerMiddleware} from "react-router-redux";
 
 import todoApp from "./reducers";
 import App from "./components/App";
@@ -17,7 +18,12 @@ const store = createStore(
 
 render(
   <Provider store={store}>
-    <App />
+      <ConnectedRouter history={history}>
+          <Switch>
+              <Route exact path='/' component={App}/>
+              <Route path='/:filter' component={App}/>
+          </Switch>
+      </ConnectedRouter>
   </Provider>,
   document.getElementById('app')
 );
