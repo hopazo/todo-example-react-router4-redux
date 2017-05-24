3f1d74a6889ddca4807c69cb06c621700ad81188
Hector Opazo <hopazo@pred.cl>
2017-05-24 13:05:12 -0400

    Incluir react-router-redux v5 como dependencia (bindings para react-router v4)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
+import {routerReducer} from "react-router-redux";
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
 const todoApp = combineReducers({
-  todos,
-  visibilityFilter
+    todos,
+    visibilityFilter,
+        router: routerReducer
 });
 
 export default todoApp
